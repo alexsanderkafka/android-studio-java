740057242d476f9bf54176007b9708f2fcee4388
Alexsander Kafka <alexsanderkafka2001alex@gmail.com>
2023-06-01 11:02:02 -0300

    Iniciado o código do app jokenpô

--- Jokenpo/app/src/main/java/com/alex/teste/jokenpo/MainActivity.java
+++ Jokenpo/app/src/main/java/com/alex/teste/jokenpo/MainActivity.java
@@ -3,6 +3,8 @@ package com.alex.teste.jokenpo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void selecionadoPedra(View view){
+        this.opcaoSelecionada("pedra");
+    }
+
+    public void selecionadoPapel(View view){
+        this.opcaoSelecionada("papel");
+    }
+
+    public void selecionadoTesoura(View view){
+        this.opcaoSelecionada("tesoura");
+    }
+
+    public void opcaoSelecionada(String opcaoSelecionada){
+        TextView texto = findViewById(R.id.textResult);
+        texto.setText(opcaoSelecionada);
     }
 }
