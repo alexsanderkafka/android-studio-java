acfff3b1c2040c47d1925b46817e42ac3ed065fb
Alexsander Kafka <alexsanderkafka2001alex@gmail.com>
2023-06-05 07:52:22 -0300

    Utilizando checkBox, valores true e false, regastando seus valores

--- Componentebasicosdeinterface/app/src/main/java/com/alex/teste/componenteBasicosDeInterface/MainActivity.java
+++ Componentebasicosdeinterface/app/src/main/java/com/alex/teste/componenteBasicosDeInterface/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -15,6 +16,7 @@ public class MainActivity extends AppCompatActivity {
     private EditText nomeText;
     private TextInputEditText emailText;
     private TextView textView;
+    private CheckBox checkVerde, checkBranco, checkVermelho;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,14 +26,42 @@ public class MainActivity extends AppCompatActivity {
         nomeText = findViewById(R.id.textNome);
         emailText = findViewById(R.id.textEmail);
         textView = findViewById(R.id.textResultado);
+
+        checkVerde = findViewById(R.id.checkVerde);
+        checkBranco = findViewById(R.id.checkBranco);
+        checkVermelho = findViewById(R.id.checkVermelho);
     }
 
     @SuppressLint("SetTextI18n")
+    public void checbox(){
+
+        String texto = "";
+        if (checkVerde.isChecked()){
+            //String cor = checkVerde.getText().toString();
+            //texto = cor;
+
+            texto = "Verde selecionado - ";
+        }
+        if (checkBranco.isChecked()){
+            texto += "Branco selecionado - ";
+        }
+        if(checkVermelho.isChecked()){
+            texto += "Vermelho selecionado";
+        }
+
+        textView.setText(texto);
+    }
+
     public void enviar(View view){
+        /*
         String nome = nomeText.getText().toString();
         String email = emailText.getText().toString();
 
         textView.setText("Nome: " + nome + "--- Email: " + email);
+         */
+
+        checbox();
+
     }
 
     public void limpar(View view){
