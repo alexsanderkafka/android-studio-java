0030c963cbcd214ade997a20b63383989da92bb4
Alexsander Kafka <alexsanderkafka2001alex@gmail.com>
2023-06-09 09:56:20 -0300

    Criado o app Gorjeta e feito o seu layout

---
+++ Calculadoradegorjeta/app/src/main/java/com/alex/teste/calculadoradegorjeta/MainActivity.java
@@ -0,0 +1,20 @@
+package com.alex.teste.calculadoradegorjeta;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+import com.google.android.material.textfield.TextInputLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
