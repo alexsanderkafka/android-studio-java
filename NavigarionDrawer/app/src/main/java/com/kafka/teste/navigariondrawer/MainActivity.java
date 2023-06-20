6e95039b21a06cd6c69210fd8d6312c3592abe33
Alexsander Kafka <alexsanderkafka2001alex@gmail.com>
2023-06-20 14:45:12 -0300

    Adicionando elementos do navigation drawer e edição

--- NavigarionDrawer/app/src/main/java/com/kafka/teste/navigariondrawer/MainActivity.java
+++ NavigarionDrawer/app/src/main/java/com/kafka/teste/navigariondrawer/MainActivity.java
@@ -42,8 +42,7 @@ public class MainActivity extends AppCompatActivity {
 
         //Define configurações do NavigationDrawer
         mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow,
-                R.id.nav_tools, R.id.nav_share, R.id.nav_send)
+                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow, R.id.nav_share, R.id.nav_send, R.id.nav_contato)
                 .setDrawerLayout(drawer)
                 .build();
 
