3062aba8786093cab335825bbea8f7e8642126f0
Alexsander Kafka <alexsanderkafka2001alex@gmail.com>
2023-06-22 14:19:56 -0300

    Adicionado conteúdo na página sobre utilizando a biblioteca about page https://github.com/medyo/android-about-page

--- AppEmpresadeConsultoria/app/src/main/java/com/kafka/teste/appempresadeconsultoria/ui/sobre/SobreFragment.java
+++ AppEmpresadeConsultoria/app/src/main/java/com/kafka/teste/appempresadeconsultoria/ui/sobre/SobreFragment.java
@@ -11,6 +11,9 @@ import android.view.ViewGroup;
 
 import com.kafka.teste.appempresadeconsultoria.R;
 
+import mehdi.sakout.aboutpage.AboutPage;
+import mehdi.sakout.aboutpage.Element;
+
 /**
  * A simple {@link Fragment} subclass.
  */
@@ -25,8 +28,32 @@ public class SobreFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_sobre, container, false);
+
+        //return inflater.inflate(R.layout.fragment_sobre, container, false);
+
+        String descricao = "A ATM consultoria tem como missão apoiar organizações que desejam" +
+                           " alcançar o sucesso atréves da excelência em gestão e da busca pela " +
+                           " qualidade";
+
+        Element versao = new Element();
+        versao.setTitle("Versão 1.0");
+
+        return new AboutPage(getActivity())
+                .setImage(R.drawable.logo)
+                .setDescription(descricao)
+                .addGroup("Entre em contato")
+                .addEmail("atendimento@atmconsultoria.com.br", "Envie um e-mail")
+                .addWebsite("https://www.google.com/", "Acesse nosso site")
+                .addGroup("Redes Sociais")
+                .addFacebook("atmconsultoria", "Facebook")
+                .addInstagram("atmconsultoria", "Instagram")
+                .addTwitter("atmconsultoria", "Twitter")
+                .addYoutube("atmconsultoria", "YouTube")
+                .addGitHub("atmconsultoria", "GitHub")
+                .addPlayStore("com.facebook.katana", "Download App")
+                .addItem(versao)
+                .create();
+
     }
 
 }
