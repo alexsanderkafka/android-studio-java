5c6a1c5fecf2b7c252f4e20dbd8197286d05e13c
Alexsander Kafka <alexsanderkafka2001alex@gmail.com>
2023-06-23 14:17:03 -0300

    Execução de música

---
+++ MediaPlayer/app/src/main/java/com/alex/teste/mediaplayer/MainActivity.java
@@ -0,0 +1,29 @@
+package com.alex.teste.mediaplayer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.SeekBar;
+
+public class MainActivity extends AppCompatActivity {
+
+    private SeekBar seekVolume;
+    private MediaPlayer mediaPlayer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mediaPlayer = MediaPlayer.create(getApplicationContext(), R.raw.teste);
+    }
+
+    public void executarSom(View view){
+        if (mediaPlayer != null){
+            mediaPlayer.start();
+        }
+    }
+}
