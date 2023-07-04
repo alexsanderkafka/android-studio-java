5da0ffe09f75c19da0af8a067a88fa182973e8ac
alexsander kafka <alexsanderkafka2001alex@gmail.com>
2023-07-04 08:20:56 -0300

    Usando o DROP, UPDATE, DELETE e uso de id na tabela

--- BancosDeDadosSQLite/app/src/main/java/com/example/bancosdedadossqlite/MainActivity.java
+++ BancosDeDadosSQLite/app/src/main/java/com/example/bancosdedadossqlite/MainActivity.java
@@ -19,11 +19,20 @@ public class MainActivity extends AppCompatActivity {
             SQLiteDatabase bancosDados = openOrCreateDatabase("app", MODE_PRIVATE, null);
 
             //Criar tabela
-            bancosDados.execSQL("CREATE TABLE IF NOT EXISTS pessoas(nome VARCHAR, idade INT(3))");
+            bancosDados.execSQL("CREATE TABLE IF NOT EXISTS pessoas(id INTEGER PRIMARY KEY AUTOINCREMENT, nome VARCHAR, idade INT(3))");
+            //bancosDados.execSQL("DROP TABLE pessoas"); apaga a tabela
+
 
             //Inserir dados
-            //bancosDados.execSQL("INSERT INTO pessoas(nome, idade) VALUES('Mario', 40)");
-            //bancosDados.execSQL("INSERT INTO pessoas(nome, idade) VALUES('Pedro', 31)");
+            //bancosDados.execSQL("INSERT INTO pessoas(nome, idade) VALUES('Mariana', 18)");
+            //bancosDados.execSQL("INSERT INTO pessoas(nome, idade) VALUES('Maria', 35)");
+
+            //fazendo um update dos dados
+            //bancosDados.execSQL("UPDATE pessoas SET idade = 55, nome = 'Mariana Silva' WHERE id = 3");
+
+            //Exclui do banco de dados
+            //bancosDados.execSQL("DELETE FROM pessoas WHERE id = 2");
+            //bancosDados.execSQL("DELETE FROM pessoas"); exclui todos os dados da tabela
 
             //Recuperar pessoas
             /*String consulta = "SELECT nome, idade FROM pessoas " +
@@ -42,22 +51,24 @@ public class MainActivity extends AppCompatActivity {
               String consulta = "SELECT nome, idade FROM pessoas " +
                               "WHERE nome LIKE '%" + filtro + "%'  ";*/
 
-            String consulta = "SELECT nome, idade FROM pessoas " +
-                              "WHERE 1=1 ORDER BY idade ASC LIMIT 3"; //ASC ou DESC
+            String consulta = "SELECT * FROM pessoas " +
+                              "WHERE 1=1 "; //ASC ou DESC
 
             Cursor cursor = bancosDados.rawQuery(consulta, null);
 
             //Indices da tabela
             int indiceNome = cursor.getColumnIndex("nome");
             int indiceIdade = cursor.getColumnIndex("idade");
+            int indiceId = cursor.getColumnIndex("id");
 
             cursor.moveToFirst();
 
             while(cursor != null){
                 String nome = cursor.getString(indiceNome);
                 String idade = cursor.getString(indiceIdade);
+                String id = cursor.getString(indiceId);
 
-                Log.i("RESULTADO - nome ", nome + " / idade: " + idade);
+                Log.i("RESULTADO - id ", id + " / nome: " + nome + " / idade: " + idade);
                 cursor.moveToNext();
             }
         }
