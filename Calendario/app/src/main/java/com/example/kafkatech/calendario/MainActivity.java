6c7964ea7a5ba724934d07fe0017059ea0480603
alexsander kafka <alexsanderkafka2001alex@gmail.com>
2023-07-24 11:51:06 -0300

    Usando um calendarView de uma biblioteca do git: https://github.com/prolificinteractive/material-calendarview

---
+++ Calendario/app/src/main/java/com/example/kafkatech/calendario/MainActivity.java
@@ -0,0 +1,61 @@
+package com.example.kafkatech.calendario;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.CalendarView;
+
+import com.prolificinteractive.materialcalendarview.CalendarDay;
+import com.prolificinteractive.materialcalendarview.MaterialCalendarView;
+import com.prolificinteractive.materialcalendarview.OnDateSelectedListener;
+import com.prolificinteractive.materialcalendarview.OnMonthChangedListener;
+
+public class MainActivity extends AppCompatActivity {
+
+    //private CalendarView calendarView;
+    private MaterialCalendarView calendarView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        calendarView = findViewById(R.id.calendarView);
+
+        /*
+        calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(@NonNull CalendarView calendarView, int year, int month, int dayOfMonth) {
+                Log.i("data: ", "Valor : " + dayOfMonth + "/" + month + "/" + year);
+            }
+        });
+
+        calendarView.setOnDateChangedListener(new OnDateSelectedListener() {
+            @Override
+            public void onDateSelected(@NonNull MaterialCalendarView widget, @NonNull CalendarDay date, boolean selected) {
+                Log.i("data: ", "Valor : " + date);
+            }
+        });*/
+
+        /*calendarView.state().edit()
+                .setMinimumDate(CalendarDay.from(2015, 1, 1))
+                .setMaximumDate(CalendarDay.from(2018, 3, 1))
+                .commit();*/
+
+        String[] meses = {"Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"};
+        calendarView.setTitleMonths(meses);
+
+        String[] dias = {"Segundo", "Terça", "Quarta", "Quinta", "Sexta", "Sábado", "Domingo"};
+        calendarView.setWeekDayLabels(dias);
+
+        calendarView.setOnMonthChangedListener(new OnMonthChangedListener() {
+            @Override
+            public void onMonthChanged(MaterialCalendarView widget, CalendarDay date) {
+                Log.i("data: ", "Valor : " + (date.getMonth() + 1) + "/" + date.getYear());
+            }
+        });
+
+    }
+}
