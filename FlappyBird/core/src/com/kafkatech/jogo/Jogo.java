32eeb15d81255ae2f20737c31464e99f9db0015b
alexsander kafka <alexsanderkafka2001alex@gmail.com>
2023-11-02 10:43:09 -0300

    Colocando os png dos canos e configuração deles

--- FlappyBird/core/src/com/kafkatech/jogo/Jogo.java
+++ FlappyBird/core/src/com/kafkatech/jogo/Jogo.java
@@ -8,11 +8,12 @@ import com.badlogic.gdx.utils.ScreenUtils;
 
 public class Jogo extends ApplicationAdapter {
 
-	private int movimentoX = 0;
-	private int movimentoY = 0;
+	//Texturas
 	private SpriteBatch batch;
 	private Texture[] passaros;
 	private Texture fundo;
+	private Texture canoBaixo;
+	private Texture canoTopo;
 
 	//Configurações
 	private float larguraDispotivo;
@@ -20,34 +21,27 @@ public class Jogo extends ApplicationAdapter {
 	private float variacao = 0;
 	private float gravidade = 0;
 	private float posicaoPassaroY = 0;
+	private float posicaoCanoX;
+	private float posicaoCanoY;
+	private float espacoEntreCanos;
 
 
 	@Override
 	public void create () {
 		//Gdx.app.log("create", "Jogo iniciado");
 
-		batch = new SpriteBatch();
-
-		passaros = new Texture[3];
-
-		passaros[0] = new Texture("passaro1.png");
-		passaros[1] = new Texture("passaro2.png");
-		passaros[2] = new Texture("passaro3.png");
-		fundo = new Texture("fundo.png");
-
-		larguraDispotivo = Gdx.graphics.getWidth();
-		alturaDispotivo = Gdx.graphics.getHeight();
-
-		posicaoPassaroY = alturaDispotivo / 2;
+		inicializarTexturas();
+		inicializarObjetos();
 	}
 
 	@Override
 	public void render () {
-		batch.begin();
+		verificarEstadoJodo();
 
-		if(variacao > 3) {
-			variacao = 0;
-		}
+		desenharTexturas();
+	}
+
+	private void verificarEstadoJodo(){
 
 		/*Aplica evento de toque de tela*/
 		boolean toqueTela = Gdx.input.justTouched();
@@ -59,16 +53,53 @@ public class Jogo extends ApplicationAdapter {
 		if(posicaoPassaroY > 0 || toqueTela) {
 			posicaoPassaroY = posicaoPassaroY - gravidade;
 		}
-		batch.draw(fundo, 0, 0, larguraDispotivo, alturaDispotivo);
-		batch.draw(passaros[(int) variacao], 30, posicaoPassaroY);
 
 		variacao += Gdx.graphics.getDeltaTime() * 10;
 
-		gravidade++;
-		movimentoX++;
-		movimentoY++;
+		/* Verifica variação para bater asas do pássaro*/
+		if(variacao > 3) {
+			variacao = 0;
+		}
 
+		gravidade++;
+	}
+
+	private void desenharTexturas(){
+		batch.begin();
+
+		batch.draw(fundo, 0, 0, larguraDispotivo, alturaDispotivo);
+		batch.draw(passaros[(int) variacao], 30, posicaoPassaroY);
+
+		//posicaoCanoX--;
+
+		batch.draw(canoBaixo, posicaoCanoX - 100, alturaDispotivo / 2 - canoBaixo.getHeight() - espacoEntreCanos / 2);
+		batch.draw(canoTopo, posicaoCanoX - 100, alturaDispotivo / 2 + espacoEntreCanos / 2);
 		batch.end();
+	}
+
+	private void inicializarTexturas(){
+		passaros = new Texture[3];
+		passaros[0] = new Texture("passaro1.png");
+		passaros[1] = new Texture("passaro2.png");
+		passaros[2] = new Texture("passaro3.png");
+
+		fundo = new Texture("fundo.png");
+
+		canoBaixo = new Texture("cano_baixo_maior.png");
+		canoTopo = new Texture("cano_topo_maior.png");
+	}
+
+	private void inicializarObjetos(){
+		batch = new SpriteBatch();
+
+		larguraDispotivo = Gdx.graphics.getWidth();
+		alturaDispotivo = Gdx.graphics.getHeight();
+
+		posicaoPassaroY = alturaDispotivo / 2;
+
+		posicaoCanoX = larguraDispotivo;
+
+		espacoEntreCanos = 200;
 	}
 	
 	@Override
