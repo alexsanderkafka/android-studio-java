576f324d87cda404f79bccb489dd7cffaf3d7c30
alexsander kafka <alexsanderkafka2001alex@gmail.com>
2023-11-06 14:19:54 -0300

    Colocando a contagem de pontos

--- FlappyBird/core/src/com/kafkatech/jogo/Jogo.java
+++ FlappyBird/core/src/com/kafkatech/jogo/Jogo.java
@@ -2,7 +2,9 @@ package com.kafkatech.jogo;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
 
@@ -27,6 +29,11 @@ public class Jogo extends ApplicationAdapter {
 	private float posicaoCanoY;
 	private float espacoEntreCanos;
 	private Random random;
+	private int pontos = 0;
+	private boolean passouCano;
+
+	//Exibição de textos
+	BitmapFont textoPontuacao;
 
 
 	@Override
@@ -40,9 +47,10 @@ public class Jogo extends ApplicationAdapter {
 	@Override
 	public void render () {
 		verificarEstadoJodo();
-
+		validarPontos();
 		desenharTexturas();
 	}
+
 
 	private void verificarEstadoJodo(){
 
@@ -51,11 +59,12 @@ public class Jogo extends ApplicationAdapter {
 		if(posicaoCanoX < -canoBaixo.getWidth()){
 			posicaoCanoX = larguraDispotivo;
 			posicaoCanoY = random.nextInt(400) - 200;
+			passouCano = false;
 		}
 		/*Aplica evento de toque de tela*/
 		boolean toqueTela = Gdx.input.justTouched();
 		if(toqueTela){
-			gravidade = -20;
+			gravidade = -14;
 		}
 
 		/* Aplica gravidade no pássaro */
@@ -77,10 +86,13 @@ public class Jogo extends ApplicationAdapter {
 		batch.begin();
 
 		batch.draw(fundo, 0, 0, larguraDispotivo, alturaDispotivo);
-		batch.draw(passaros[(int) variacao], 30, posicaoPassaroY);
+		batch.draw(passaros[(int) variacao], 50, posicaoPassaroY);
 
 		batch.draw(canoBaixo, posicaoCanoX, alturaDispotivo / 2 - canoBaixo.getHeight() - espacoEntreCanos / 2 + posicaoCanoY);
 		batch.draw(canoTopo, posicaoCanoX, alturaDispotivo / 2 + espacoEntreCanos / 2 + posicaoCanoY);
+
+		//DESENHA O TEXTO
+		textoPontuacao.draw(batch, String.valueOf(pontos), larguraDispotivo / 2, alturaDispotivo - 110);
 
 		batch.end();
 	}
@@ -95,6 +107,11 @@ public class Jogo extends ApplicationAdapter {
 
 		canoBaixo = new Texture("cano_baixo_maior.png");
 		canoTopo = new Texture("cano_topo_maior.png");
+
+		//Configuração dos textos
+		textoPontuacao = new BitmapFont();
+		textoPontuacao.setColor(Color.WHITE);
+		textoPontuacao.getData().setScale(10);
 	}
 
 	private void inicializarObjetos(){
@@ -109,6 +126,15 @@ public class Jogo extends ApplicationAdapter {
 		posicaoCanoX = larguraDispotivo;
 
 		espacoEntreCanos = 200;
+	}
+
+	private void validarPontos() {
+		if(posicaoCanoX < 50 - passaros[0].getWidth()){//Passou da posição do passaro
+			if(!passouCano){
+				pontos++;
+				passouCano = true;
+			}
+		}
 	}
 	
 	@Override
